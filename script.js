8f843424d185319beed370771b01adcc2563f5f8
ESAKKI RAJ K <118879057+ESAKKIRAJK@users.noreply.github.com>
2022-11-26 21:17:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+
+// Use the restcountries API Print the all country names,region,subregion using for loop.
+// var request = new XMLHttpRequest();
+// request.open ("GET", "https://restcountries.com/v3.1/all",true)
+// request.send()
+// request.onload = function() {
+//     var data = request.response
+//     var result = JSON.parse(data)
+//     for(var i = 0; i<result.length; i++) {
+//         console.log(result[i].region,result[i].subregion,result[i].name.common);
+//     }
+// }
+
+
+// Question no: 1 how to compare two json objects have same properties without order.
+// var obj1 = {
+//     "name" : "Person 1",
+//     "Age" : "5"
+// }
+// var obj2 = {
+//     "Age" : "5",
+//     "name" : "Person 1"
+// }
+// JSON.stringify(obj1) === JSON.stringify(obj2);
+
+// Question no: 2 Use the restcountries API print the all country names,region,subregion and population.
+// var request = new XMLHttpRequest();
+// request.open ("GET", "https://restcountries.com/v3.1/all",true)
+// request.send()
+// request.onload = function() {
+//     var data = request.response
+//     var result = JSON.parse(data)
+//     for(var obj of result){
+//         console.log(obj.name.common,obj.region,obj.subregion,obj.population);}
+//     }
+
+
+
+//Question no: 2 Use the restcountries API print the all country flags in console.
+
+// var request = new XMLHttpRequest();
+// request.open ("GET", "https://restcountries.com/v3.1/all",true)
+// request.send()
+// request.onload = function() {
+//     var data = request.response
+//     var result = JSON.parse(data)
+//     for(var obj of result){
+//         console.log(obj.flags.png);}
+//     }
+
+
+
+
+
+
